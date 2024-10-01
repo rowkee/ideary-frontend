e384c027e8fb0ea83c1bc69f8980d4f30eaca9f6
rowkee <rick.keenan10@gmail.com>
2024-10-01 11:39:39 +0100

    added login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </Router>
---
+++ src/pages/SignUp.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Sign Up</h3>
+      <label>Email:</label>
+      <input
+        type="email"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        value={email}
+      ></input>
+      <label>Password:</label>
+      <input
+        type="password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        value={password}
+      ></input>
+      <button>Signup</button>
+    </form>
+  );
+};
+
+export default Signup;
