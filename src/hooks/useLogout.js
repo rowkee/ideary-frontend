ec08fe6bd16d7dcbd19d68e589afa9b2701b68e7
rowkee <rick.keenan10@gmail.com>
2024-10-09 10:44:16 +0100

    small changes

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,11 +1,14 @@
 import useAuthContext from "./useAuthContext";
+import { useIdeasContext } from "./useIdeasContext";
 
 const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: ideasDispatch } = useIdeasContext();
 
   const logout = () => {
     localStorage.removeItem("user");
     dispatch({ type: "LOGOUT" });
+    ideasDispatch({ type: "SET_IDEAS", payload: null });
   };
 
   return { logout };
