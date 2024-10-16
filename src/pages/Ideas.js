75fc81d24b70df624f1f8abf0881873bcd4a2250
rowkee <rick.keenan10@gmail.com>
2024-10-16 12:32:33 +0100

    fixed myaccount page

--- src/pages/Ideas.js
+++ src/pages/Ideas.js
@@ -1,47 +1,37 @@
 import React from "react";
 import { useEffect } from "react";
 import { useIdeasContext } from "../hooks/useIdeasContext";
-import useAuthContext from "../hooks/useAuthContext";
 
 // components
 import IdeaCard from "../components/IdeaCard";
 
-function MyAccount() {
+function Ideas() {
   const { ideas, dispatch } = useIdeasContext();
-  const { user } = useAuthContext();
 
   useEffect(() => {
     const fetchIdeas = async () => {
       const response = await fetch(
-        `${process.env.REACT_APP_API_BASE_URL}api/ideas/account?userId=${user.id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${user.token}`,
-          },
-        }
+        `${process.env.REACT_APP_API_BASE_URL}api/ideas`
       );
       const json = await response.json();
-      console.log(json);
       if (response.ok) {
         dispatch({ type: "SET_IDEAS", payload: json });
       }
     };
-    if (user) {
-      fetchIdeas();
-    }
-  }, [dispatch, user]);
+    fetchIdeas();
+  }, [dispatch]);
 
   return (
     <div>
-      <h1 className="text-4xl font-bold">My Posts</h1>
-      <div className="ideas">
+      <h1 className="text-4xl font-bold">Ideas</h1>
+      <div className="flex flex-auto flex-wrap">
         {ideas &&
           ideas.map((idea) => (
-            <IdeaCard key={idea._id} idea={idea} showDeleteButton={true} />
+            <IdeaCard key={idea._id} idea={idea} showDeleteButton={false} />
           ))}
       </div>
     </div>
   );
 }
 
-export default MyAccount;
+export default Ideas;
